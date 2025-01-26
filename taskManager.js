6f9872b141156a0428b5f460a8058790a2de17ed
Fahim <fahim.mun07@proton.me>
2025-01-26 19:05:38 +0600

    First Commit

---
+++ taskManager.js
@@ -0,0 +1,26 @@
+// Collecting the necessary Objects from the DOM
+const taskTitle = document.getElementById("taskTitle");
+const taskDescription = document.getElementById("taskDescription");
+const taskPriority = document.getElementById("taskPriority");
+const submitBtn = document.getElementsByTagName("button")
+const taskList = document.getElementById("taskList");
+
+// Add Task
+const addTask = (title, description, priority) => {
+
+};
+
+// Delete Task
+const deleteTask = (taskId) => {
+
+};
+
+// Update Task
+const updateTask = (taskId, updates) => {
+
+};
+
+// Toggle Task Completion
+const toggleTaskCompletion = (taskId) => {
+    
+};
